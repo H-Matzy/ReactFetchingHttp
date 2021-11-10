d4d37b8ba3187d2f5e2cf61cbcccaa977479e6d7
HMatz <HMatz@midwayusa.com>
2021-11-10 11:57:17 -0600

    Updates

---
+++ precache-manifest.c5c0c7ed3b81e0ad9374a5a8af30134f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c620304ef5c72faf732ad532563a06a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2395c5258f940a24bfb0",
+    "url": "/static/css/main.ec400db7.chunk.css"
+  },
+  {
+    "revision": "ca77627b6a30990c1a89",
+    "url": "/static/js/2.3c152af1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.3c152af1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2395c5258f940a24bfb0",
+    "url": "/static/js/main.9e157a61.chunk.js"
+  },
+  {
+    "revision": "f13d5fb20258b992088c",
+    "url": "/static/js/runtime-main.a6b2f021.js"
+  }
+]);
